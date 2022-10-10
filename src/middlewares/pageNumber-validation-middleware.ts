c76836fe7a51245baa98d041b820937a1651bd66
AlexGr <753464@gmail.com>
2022-10-10 16:05:45 +0300

    1.0.6

--- src/middlewares/pageNumber-validation-middleware.ts
+++ src/middlewares/pageNumber-validation-middleware.ts
@@ -2,9 +2,10 @@ import { param, query } from 'express-validator';
 
 export const pageNumberQueryValidationMiddleware = query('pageNumber')
     .default(1)
-    .custom((value: string) => {
-        return +value;
-    })
+    // .custom((value: string) => {
+    //     return +value;
+    // })
+    .toInt()
     .isNumeric()
     .exists()
 
--- src/middlewares/pageSize-validation-middleware.ts
+++ src/middlewares/pageSize-validation-middleware.ts
@@ -2,9 +2,10 @@ import { param, query } from 'express-validator';
 
 export const pageSizeQueryValidationMiddleware = query('pageSize')
     .default(10)
-    .custom((value: string) => {
-        return +value;
-    })
+    .toInt()
+    // .custom((value: string) => {
+    //     return +value;
+    // })
     .isNumeric()
     .exists()
 
